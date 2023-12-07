477a2ab0bc33ff4bc4fa45aea69757f4c8901a9b
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-12-07 10:34:10 +0800

    update

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -49,6 +49,13 @@ export default function Portfolio() {
     }
     setIndex(index - 1);
   };
+
+  const imageLinks = [
+    "https://mini-project-2-nine.vercel.app/",
+    "https://ricohermosoryan.github.io/Mini-Project-1/HeroHaven.html",
+    "https://ricohermosoryan.github.io/zalora-clone/zalorasample.html",
+  ];
+
   return (
     <>
       <PageTransition>
@@ -61,17 +68,23 @@ export default function Portfolio() {
             <div className="container flex">
               <div className=" m-auto aspect-[16/9] relative overflow-hidden rounded-[16px] w-[120vh]">
                 <AnimatePresence initial={false} custom={direction}>
-                  <motion.img
-                    variants={variants}
-                    animate="animate"
-                    initial="initial"
-                    exit="exit"
-                    key={images[index]}
-                    src={images[index]}
-                    alt="images"
-                    className=" absolute top-0 left-0 w-[100%] h-[100%] object-cover object-center"
-                    custom={direction}
-                  />
+                  <a
+                    href={imageLinks[index]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <motion.img
+                      variants={variants}
+                      animate="animate"
+                      initial="initial"
+                      exit="exit"
+                      key={images[index]}
+                      src={images[index]}
+                      alt="images"
+                      className=" absolute top-0 left-0 w-[100%] h-[100%] object-cover object-center"
+                      custom={direction}
+                    />
+                  </a>
                 </AnimatePresence>
                 <button
                   className="p-2 bg-white rounded-full hover:bg-slate-200 absolute top-[50%] left-3"
