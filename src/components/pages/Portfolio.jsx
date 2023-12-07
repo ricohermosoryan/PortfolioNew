3803d8d0c0de7bc94e1456536ffc68e41f0c5144
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-12-07 10:09:56 +0800

    update

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTransition from "../PageTransition";
+import image1 from "../../assets/image.svg";
+import image2 from "../../assets/image2.svg";
+import image3 from "../../assets/image3.svg";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { AnimatePresence, motion } from "framer-motion";
+
+const images = [image1, image2, image3];
+
+const variants = {
+  initial: {
+    x: 200,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+  },
+  exit: {
+    x: -200,
+    opacity: 0,
+  },
+};
 
 export default function Portfolio() {
+  const [index, setIndex] = useState(0);
+
+  const nextStep = () => {
+    if (index === images.length - 1) {
+      setIndex(0);
+      return;
+    }
+    setIndex(index + 1);
+  };
+
+  const prevStep = () => {
+    if (index === 0) {
+      setIndex(images.length - 1);
+      return;
+    }
+    setIndex(index - 1);
+  };
   return (
     <>
       <PageTransition>
@@ -11,8 +50,33 @@ export default function Portfolio() {
             <p className="text-[20px] font-normal text-center pt-8 pb-4">
               My Projects
             </p>
-            <div>
-              <img src="/src/assets/Quantum Galaxy.PNG" alt="image" />
+            <div className="container flex">
+              <div className=" m-auto aspect-[16/9] relative overflow-hidden rounded-[16px] w-[120vh]">
+                <AnimatePresence>
+                  <motion.img
+                    variants={variants}
+                    animate="animate"
+                    initial="initial"
+                    exit="exit"
+                    key={images[index]}
+                    src={images[index]}
+                    alt="images"
+                    className=" absolute top-0 left-0 w-[100%] h-[100%] object-cover object-center"
+                  />
+                </AnimatePresence>
+                <button
+                  className="p-2 bg-white rounded-full hover:bg-slate-200 absolute top-[50%] left-3"
+                  onClick={prevStep}
+                >
+                  <FaChevronLeft />
+                </button>
+                <button
+                  className="p-2 bg-white rounded-full hover:bg-slate-200 absolute top-[50%] right-3"
+                  onClick={nextStep}
+                >
+                  <FaChevronRight />
+                </button>
+              </div>
             </div>
           </div>
         </div>
