75842f63e9c19477a1afcba135e3ea4bf4ca8fa3
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-12-16 14:21:08 +0800

    update

--- src/components/ParticlesContainer.jsx
+++ src/components/ParticlesContainer.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 export default function ParticlesContainer() {
@@ -16,7 +16,7 @@ export default function ParticlesContainer() {
       // starting from v2 you can add only the features you need reducing the bundle size
       //await loadAll(engine);
       //await loadFull(engine);
-      await loadSlim(engine);
+      await loadFull(engine);
       //await loadBasic(engine);
     }).then(() => {
       setInit(true);
@@ -52,7 +52,7 @@ export default function ParticlesContainer() {
             quantity: 90,
           },
           repulse: {
-            distance: 300,
+            distance: 150,
             duration: 0.4,
           },
         },
@@ -63,7 +63,7 @@ export default function ParticlesContainer() {
         },
         links: {
           color: "#87ceeb",
-          distance: 150,
+          distance: 90,
           enable: true,
           opacity: 0.5,
           width: 1,
@@ -86,7 +86,7 @@ export default function ParticlesContainer() {
             enable: true,
             area: 800,
           },
-          value: 80,
+          value: 250,
         },
         opacity: {
           value: 0.5,
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import PageTransition from "../PageTransition";
 import { AnimatePresence, motion } from "framer-motion";
-import profile from "../../assets/profilenew.png";
+import profile from "../../assets/profile2.jpg";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   const text1 = "Hi,";
   const text2 = "I'm  Ryan ";
   const text3 = "web  Developer";
+
+  const socialMedia = [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/ryan.ricohermoso/",
+      icons: <FaFacebook />,
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/ricohermosoryan",
+      icons: <FaGithub />,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/ryan-ricohermoso-25440227a/",
+      icons: <FaLinkedin />,
+    },
+    {
+      name: "Twitter",
+      href: "https://twitter.com/RaiRicohermoso",
+      icons: <FaSquareXTwitter />,
+    },
+  ];
 
   const spanVariants = {
     visible: { y: 0, scaleY: 1 },
@@ -49,7 +76,7 @@ export default function Home() {
         <section className="flex items-center justify-center flex-col gap-12 relative">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
             <div className="w-full h-full flex flex-col items-center lg:items-center justify-center gap-4">
-              <div className="text-left w-[300px] mt-[100px] md:w-[600px] lg:ms-[300px] lg:w-[650px] lg:mt-[180px] md:mt-[70px]">
+              <div className="text-left w-[300px] mt-[100px] md:w-[600px] lg:ms-[300px] lg:w-[650px] lg:mt-[150px] md:mt-[70px]">
                 <motion.h1
                   className=" text-[2.5rem] md:text-[4.5rem] lg:text-[5.5rem] font-semibold"
                   variants={list}
@@ -139,7 +166,8 @@ export default function Home() {
                     </motion.span>
                   ))}
                 </motion.h1>
-                <div className="mt-[30px]">
+                {/* Introduction */}
+                <div className="mt-[10px]">
                   <p className="w-[300px] md:w-[590px] text-[10px] md:text-[20px] lg:w-[1200px]">
                     I'm a versatile Full Stack Developer with a passion for
                     crafting innovative web solutions. With a strong foundation
@@ -150,14 +178,29 @@ export default function Home() {
                     exceptional user experience.
                   </p>
                 </div>
+                {/* Social Media Icons */}
+                <div className="flex gap-3 md:gap-4 pt-3 md:pt-5">
+                  {socialMedia.map((item, i) => (
+                    <div
+                      key={i}
+                      className="border border-sky-500/50 p-2 md:p-3 rounded-full group"
+                    >
+                      <a href={item.href} target="blank">
+                        <span className=" text-[15px] md:text-[30px] hover:text-red-500">
+                          {item.icons}
+                        </span>
+                      </a>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
 
-            <div className="w-[200px] h-[200px] ms-[90px] lg:ms-0 md:w-[300px] md:h-[300px] md:ms-[230px] lg:w-[830px] lg:h-[700px] flex items-start justify-center lg:items-center md:mt-[20px] bg-no-repeat bg-center rounded-full -rotate-[30deg]">
+            <div className="w-[200px] h-[200px] ms-[90px] lg:ms-0 md:w-[300px] md:h-[300px] md:ms-[230px] lg:w-[830px] lg:h-[700px] flex items-start justify-center lg:items-center md:mt-[20px] rounded-full">
               <motion.img
                 src={profile}
                 alt="image"
-                className=" w-[200px] h-[200px] md:w-[290px] md:h-[290px] lg:w-[350px] lg:h-[350px] rounded-full object-cover shadow-xl  lg:absolute lg:right-[300px] lg:top-[100px] shadow-sky-500/50"
+                className=" w-[200px] h-[200px] md:w-[290px] md:h-[290px] lg:w-[350px] lg:h-[350px] rounded-full object-cover shadow-xl lg:absolute lg:right-[300px] lg:top-[100px] shadow-sky-500/50"
                 initial={{ y: 0 }}
                 animate={{ y: [-10, 10, -10] }}
                 transition={{
